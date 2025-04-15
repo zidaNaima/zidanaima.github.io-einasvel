5bd7b3c9e8883f83f8b5165e4b8627ed9f65ecdd
Naima <naima.zida8@gmail.com>
2025-04-14 21:05:49 -0700

    Footer form update (visitors can now send messages). 3D model of Mikimoh added. Planet's wildlife section made smaller/more navigable. Map of Mikimoh updated to warp less. Head anatomy and evolution tree update. Photo sizes reduced. Gallery format fixed.

--- js/guidescript.js
+++ js/guidescript.js
@@ -4,7 +4,7 @@
 var page = window.location.pathname;
 var dropdown = document.getElementsByClassName("dropdown");
 var drpdwn = dropdown[0];
-var OkIcon = window.getComputedStyle(drpdwn, '::after');
+// var OkIcon = window.getComputedStyle(drpdwn, '::after');
 switch (page) {
     case '/index.html':
     case '/zidanaima.github.io-einasvel/index.html':
@@ -33,117 +33,194 @@ switch (page) {
 }
 
 
-// Footer form validation
+
+// Footer form submission
+// https://www.youtube.com/watch?v=qxWDVRyc95E
+const form = document.forms[0];
+const messageInput = form.message;
+const contactInput = form.contact;
+
+const publicKey = "xfbPdiVD70qK8vTY3";
+const serviceId = "service_g63duhk";
+const templateId = "template_gmt1yl4";
+
+// Form validation
 function validateMessage() {
-    if (document.forms[0].message.value == "") { // no message entered
+    if (messageInput.value == "") { // no message entered
         alert("Please write a message before hitting submit.")
         return false;
     }
     return true;
 }
 
+// Form submission
+form.addEventListener("submit", (e) => {
+    emailjs.init(publicKey);
 
-// 17 sounds
-// NOTE, .ogg > .mp3
-function alphaSound(letter) {
-    var audio;
-    switch (letter) {
-        // file paths relative to the HTML file, not the JS file
-        case 'D': audio = new Audio('audio/d.ogg'); break;
-        case 'M': audio = new Audio('audio/m.ogg'); break;
-        case 'N': audio = new Audio('audio/n.ogg'); break;
-        case 'P': audio = new Audio('audio/p.ogg'); break;
-        case 'A': audio = new Audio('audio/au.ogg'); break;
-        case 'W': audio = new Audio('audio/ooh.ogg'); break;
-        case 'O': audio = new Audio('audio/oh.ogg'); break;
-        case 'E': audio = new Audio('audio/e.ogg'); break;
-        case 'I': audio = new Audio('audio/ih.ogg'); break;
-        case 'Y': audio = new Audio('audio/eih.ogg'); break;
-        case 'H': audio = new Audio('audio/hl.ogg'); break;
-        case 'G': audio = new Audio('audio/ng.ogg'); break;
-        case 'K': audio = new Audio('audio/kg.ogg'); break;
-        case 'Q': audio = new Audio('audio/q.ogg'); break;
-        case 'S': audio = new Audio('audio/s.ogg'); break;
-        case 'T': audio = new Audio('audio/th.ogg'); break;
-        case 'U': audio = new Audio('audio/uh.ogg'); break;
+    e.preventDefault(); // prevent form default behavior. Needed for outside submission.
+    const inputFields = {
+        message: messageInput.value,
+        contact: contactInput.value,
+    };
+
+    if (validateMessage()) {
+        emailjs.send(serviceId, templateId, inputFields)
+            .then(() => {
+                alert("Message sent successfully. Thank you!");
+                messageInput.value = "";
+                contactInput.value = "";
+            }, (error) => {
+                alert("Sorry, something seems to have gone wrong.");
+                console.log(error); // sure.
+            });
     }
-    audio.play();
-}
 
-// function pronounce(word) {
-//     var audio;
-//     switch (word) {
-//         case '': audio = new Audio('audio/'); break;
-//     }
-//     audio.play();
-// }
-
-
-// Tutorial:
-// https://www.youtube.com/watch?v=9HcxHDS2w1s
-const buttons = document.querySelectorAll("[data-carousel-button]");
-// data attribute is nice so you don't have to worry about class overlap
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.carouselButton == "next" ? 1 : -1;
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
-
-        const activeSlide = slides.querySelector("[data-active]");
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if (newIndex < 0) newIndex = slides.children.length - 1; // loops from front to back
-        if (newIndex >= slides.children.length) newIndex = 0; // loops from back to front
-
-        slides.children[newIndex].dataset.active = true;
-        delete activeSlide.dataset.active;
-    });
 });
 
 
-// expand and collapse one section
-// https://www.w3schools.com/howto/howto_js_collapsible.asp
-var coll = document.getElementsByClassName("collapse");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display == "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
+// --------------------------------- HOME
+
+if (page == "/index.html" || page == "/zidanaima.github.io-einasvel/index.html") {
+
+    // Tutorial:
+    // https://www.youtube.com/watch?v=9HcxHDS2w1s
+    const buttons = document.querySelectorAll("[data-carousel-button]");
+    // data attribute is nice so you don't have to worry about class overlap
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const offset = button.dataset.carouselButton == "next" ? 1 : -1;
+            const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+
+            const activeSlide = slides.querySelector("[data-active]");
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+            if (newIndex < 0) newIndex = slides.children.length - 1; // loops from front to back
+            if (newIndex >= slides.children.length) newIndex = 0; // loops from back to front
+
+            slides.children[newIndex].dataset.active = true;
+            delete activeSlide.dataset.active;
+        });
     });
+
 }
 
-// expand and collapse all sections
-var expall = document.getElementById("expand-all");
-expall.addEventListener("click", function () {
-    this.classList.toggle("active"); // makes expand-all active
-    var colla = document.getElementsByClassName("collapse");
-    for (i = 0; i < colla.length; i++) {
-        var content = colla[i].nextElementSibling;
-        if (expall.classList.contains("active")) { // expand
-            colla[i].classList.add("active"); // sets active status for all
-            content.style.display = "block";
-        } else {
-            colla[i].classList.remove("active"); // collapse
-            content.style.display = "none";
+
+
+// --------------------------------- LANGUAGE
+
+if (page == "/language.html" || page == "/zidanaima.github.io-einasvel/language.html") {
+
+    // 17 sounds
+    // NOTE, .ogg > .mp3
+    function alphaSound(letter) {
+        var audio;
+        switch (letter) {
+            // file paths relative to the HTML file, not the JS file
+            case 'D': audio = new Audio('audio/d.ogg'); break;
+            case 'M': audio = new Audio('audio/m.ogg'); break;
+            case 'N': audio = new Audio('audio/n.ogg'); break;
+            case 'P': audio = new Audio('audio/p.ogg'); break;
+            case 'A': audio = new Audio('audio/au.ogg'); break;
+            case 'W': audio = new Audio('audio/ooh.ogg'); break;
+            case 'O': audio = new Audio('audio/oh.ogg'); break;
+            case 'E': audio = new Audio('audio/e.ogg'); break;
+            case 'I': audio = new Audio('audio/ih.ogg'); break;
+            case 'Y': audio = new Audio('audio/eih.ogg'); break;
+            case 'H': audio = new Audio('audio/hl.ogg'); break;
+            case 'G': audio = new Audio('audio/ng.ogg'); break;
+            case 'K': audio = new Audio('audio/kg.ogg'); break;
+            case 'Q': audio = new Audio('audio/q.ogg'); break;
+            case 'S': audio = new Audio('audio/s.ogg'); break;
+            case 'T': audio = new Audio('audio/th.ogg'); break;
+            case 'U': audio = new Audio('audio/uh.ogg'); break;
         }
+        audio.play();
     }
-});
 
 
-// expand and collapse one INSIDE section
-var collI = document.getElementsByClassName("collapse-inside");
-for (i = 0; i < collI.length; i++) {
-    collI[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display == "block") {
-            content.style.display = "none";
+
+    // function pronounce(word) {
+    //     var audio;
+    //     switch (word) {
+    //         case '': audio = new Audio('audio/'); break;
+    //     }
+    //     audio.play();
+    // }
+
+}
+
+
+
+// --------------------------------- PLANET
+
+if (page == "/planet.html" || page == "/zidanaima.github.io-einasvel/planet.html") {
+
+    // Large-map Data
+    var mapBttn = document.getElementById("map-data-button");
+    mapBttn.addEventListener("click", () => {
+        var map = document.getElementById("large-map");
+
+        if (map.style.backgroundImage.includes("map_named.png") || map.style.backgroundImage == "") { // background img starts unset
+            map.style.backgroundImage = "url(../images/map_named_not-named.png)";
         } else {
-            content.style.display = "block";
+            map.style.backgroundImage = "url(../images/map_named.png)";
         }
     });
+
+}
+
+
+
+// --------------------------------- PEOPLE
+
+if (page == "/people.html" || page == "/zidanaima.github.io-einasvel/people.html") {
+
+    // expand and collapse one section
+    // https://www.w3schools.com/howto/howto_js_collapsible.asp
+    var coll = document.getElementsByClassName("collapse");
+    // var i;
+
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display == "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+    // expand and collapse all sections
+    var expall = document.getElementById("expand-all");
+    expall.addEventListener("click", function () {
+        this.classList.toggle("active"); // makes expand-all active
+        var colla = document.getElementsByClassName("collapse");
+        for (let i = 0; i < colla.length; i++) {
+            var content = colla[i].nextElementSibling;
+            if (expall.classList.contains("active")) { // expand
+                colla[i].classList.add("active"); // sets active status for all
+                content.style.display = "block";
+            } else {
+                colla[i].classList.remove("active"); // collapse
+                content.style.display = "none";
+            }
+        }
+    });
+
+    // expand and collapse one INSIDE section
+    var collI = document.getElementsByClassName("collapse-inside");
+    for (let i = 0; i < collI.length; i++) {
+        collI[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display == "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
 }
---
+++ js/threeD.js
@@ -0,0 +1,85 @@
+
+
+// THREE.js 3D planet
+// https://www.youtube.com/watch?v=lGokKxJ8D2c&t=98s
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+// to allow camera movement
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+// to allow the import of the gLTF
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+
+
+// NOTE: winDiv only refreshes with each page refresh
+var winDiv;
+if (window.innerWidth <= "840") { // medium sized screens !!
+    winDiv = -0.0006*window.innerWidth + 1.7;
+} else {
+    winDiv = 2;
+}
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, (window.innerWidth / winDiv) / (window.innerHeight / 1.25), 1, 50); //follows W and H of CSS window, sets near(1) and far(1000) point of camera
+camera.position.set(1, 1, 18); // so we don't default to the origin
+
+
+let object, controls;
+
+const loader = new GLTFLoader();
+// load in model
+loader.load('models/planet/mikimoh.glb',
+    function (gltf) {
+        object = gltf.scene;
+        scene.add(object)
+
+        object.position.set(1, 1, 1); // right, up, forward
+        object.scale.set(0.75, 0.75, 0.75); // Adjust scaling if the model is too large/small
+        // 0.01 for all on Venus
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.enableZoom = false;
+        controls.enablePan = false; // drag
+
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded'); // loading in progress
+    },
+    function (error) {
+        console.error(error);
+    }
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize((window.innerWidth / winDiv), (window.innerHeight / 1.25));
+renderer.setPixelRatio(window.devicePixelRatio);
+
+document.getElementById("threeD-map").appendChild(renderer.domElement);
+
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // color and intensity
+topLight.position.set(500, 500, 500); // top left-"ish"
+topLight.castShadow = true;
+scene.add(topLight);
+
+const sideLight = new THREE.DirectionalLight(0x123456, 1.75); // A softer orange light
+sideLight.position.set(-300, -200, -200);
+// scene.add(sideLight);
+
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 2); // play around with the lights!
+scene.add(ambientLight);
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", function () {
+    camera.aspect = (window.innerWidth / winDiv) / (window.innerHeight / 1.25); // window?
+    camera.updateProjectionMatrix();
+    renderer.setSize((window.innerWidth / winDiv), (window.innerHeight / 1.25))
+});
+
+// start the animation
+animate();
