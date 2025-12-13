6fe6880d270982ba6be58f1e688a7dbe2e1717f1
Naima <naima.zida8@gmail.com>
2025-12-13 02:21:30 -0800

    3D model now only loads in after the thumbnail is clicked (set to only load in the model once). window height and width changed to the height and width of the container. page no longer reloads constantly due to (presulably) a window resize glitch.

--- js/threeD.js
+++ js/threeD.js
@@ -9,85 +9,90 @@ import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/js
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 
-// NOTE: winDiv only refreshes with each page refresh
-var winDiv;
-if (window.innerWidth <= "840") { // medium sized screens !!
-    winDiv = -0.0006 * window.innerWidth + 1.7;
-    // winDiv = 1;
-} else {
-    winDiv = 2;
-}
+
+document.getElementById("threeD-thumbnail").addEventListener("click", () => {
+    document.getElementById("threeD-thumbnail").style.display = "none";
+    document.getElementById("threeD-map").style.height = "80vh";
+    document.getElementById("threeD-map").style.maxHeight = "800px";
+
+    // Initialization
+    const scene = new THREE.Scene();
+    const container = document.getElementById("threeD-map");
+    const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 1, 50); //follows W and H of CSS window, sets near(1) and far(50) point of camera
+    camera.position.set(1, 1, 18); // so we don't default to the origin
 
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, (window.innerWidth / winDiv) / (window.innerHeight / 1.25), 1, 50); //follows W and H of CSS window, sets near(1) and far(1000) point of camera
-camera.position.set(1, 1, 18); // so we don't default to the origin
+
+    let object, controls;
+
+    const loader = new GLTFLoader();
+    // Model Initialization
+    loader.load('models/planet/mikimoh.glb',
+        function (gltf) {
+            object = gltf.scene;
+            scene.add(object)
+
+            object.position.set(1, 1, 1); // right, up, forward
+            object.scale.set(0.75, 0.75, 0.75); // Adjust scaling if the model is too large/small
+            controls = new OrbitControls(camera, renderer.domElement);
+            controls.enableZoom = false;
+            controls.enablePan = false; // drag
+
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded'); // loading in progress
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
+
+    // Renderer ("inner window") size
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio); // the level of detail
+
+    document.getElementById("threeD-map").appendChild(renderer.domElement);
 
 
-let object, controls;
 
-const loader = new GLTFLoader();
-// LOAD IN MODEL
-loader.load('models/planet/mikimoh.glb',
-    function (gltf) {
-        object = gltf.scene;
-        scene.add(object)
+    // LIGHTING
+    const topLight = new THREE.DirectionalLight(0xffffff, 1); // color and intensity
+    topLight.position.set(500, 500, 500); // top left-"ish"
+    topLight.castShadow = true;
+    scene.add(topLight);
 
-        object.position.set(1, 1, 1); // right, up, forward
-        object.scale.set(0.75, 0.75, 0.75); // Adjust scaling if the model is too large/small
-        // 0.01 for all on Venus
-        controls = new OrbitControls(camera, renderer.domElement);
-        controls.enableZoom = false;
-        controls.enablePan = false; // drag
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+    scene.add(ambientLight);
 
-    },
-    function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded'); // loading in progress
-    },
-    function (error) {
-        console.error(error);
+    // const sideLight = new THREE.DirectionalLight(0x123456, 1.75); // A softer orange light
+    // sideLight.position.set(-300, -200, -200);
+    // scene.add(sideLight);
+
+
+
+    // SCREEN RESIZE
+    window.addEventListener("resize", function () {
+        camera.aspect = container.clientWidth / container.clientHeight; // update camera's aspect ratio to avoid squash and stretch
+        camera.updateProjectionMatrix(); // mandatory function call
+        renderer.setSize(container.clientWidth, container.clientHeight)
+
+        // Re-adds split screen
+        if (this.window.innerWidth > "840") {
+            window.location.reload(true);
+        }
+    });
+
+
+
+    // Allow users to interact with the model
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
     }
-);
-
-// PLANET SIZE 
-const renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize((window.innerWidth / winDiv), (window.innerHeight / 1.25));
-renderer.setPixelRatio(window.devicePixelRatio);
-
-document.getElementById("threeD-map").appendChild(renderer.domElement);
-
-// LIGHTING
-const topLight = new THREE.DirectionalLight(0xffffff, 1); // color and intensity
-topLight.position.set(500, 500, 500); // top left-"ish"
-topLight.castShadow = true;
-scene.add(topLight);
-
-const sideLight = new THREE.DirectionalLight(0x123456, 1.75); // A softer orange light
-sideLight.position.set(-300, -200, -200);
-// scene.add(sideLight);
+    // Start the animation
+    animate();
 
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 2); // play around with the lights!
-scene.add(ambientLight);
-
-
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-
-// SCREEN RESIZE
-window.addEventListener("resize", function () {
-    camera.aspect = (window.innerWidth / winDiv) / (window.innerHeight / 1.25); // window?
-    camera.updateProjectionMatrix();
-    renderer.setSize((window.innerWidth / winDiv), (window.innerHeight / 1.25))
-
-    // reload once
-    const setwindow = document.getElementById("settings-window");
-    setwindow.style.display = "none";
-    window.location.reload(true);
-});
-
-// start the animation
-animate();
+}, {once: true});
+// End Code
